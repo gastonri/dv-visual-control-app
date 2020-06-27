0617dd9923665f143e338a1e65067ddd7f2956c0
Gastón Rivata <gastonrivata@gmail.com>
2020-06-27 18:53:17 -0300

    Updates

---
+++ precache-manifest.4f5cd583a15bf26e5c83f967e9d336d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301cd4324b04dcb3f250ce80ba7051fd",
+    "url": "/dv-visual-control-app/index.html"
+  },
+  {
+    "revision": "65ebc4c9e2464e041b72",
+    "url": "/dv-visual-control-app/static/css/main.9db6a446.chunk.css"
+  },
+  {
+    "revision": "3b6bf34b44c6b5d9d05d",
+    "url": "/dv-visual-control-app/static/js/2.b774b072.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dv-visual-control-app/static/js/2.b774b072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ebc4c9e2464e041b72",
+    "url": "/dv-visual-control-app/static/js/main.3f44ffea.chunk.js"
+  },
+  {
+    "revision": "7cb87e2d2f41f3175586",
+    "url": "/dv-visual-control-app/static/js/runtime-main.2eb76539.js"
+  }
+]);
